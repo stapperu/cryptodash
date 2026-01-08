2af977698812ade990eadab37756af5dbf64bfdc
stapperu <k.kubiak94@gmail.com>
2026-01-08 19:26:41 +0100

    import fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from "react";
-import "./index.css";
+import "./src/index.css";
 const API_URL = import.meta.env.VITE_API_URL;
 import ControlNav from "./components/ControlNav";
 import { NavLink } from "react-router";
