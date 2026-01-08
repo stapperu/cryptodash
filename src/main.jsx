8968dfcc2ee6d2b7f56c4bcda27a31b8f8bddfbf
stapperu <k.kubiak94@gmail.com>
2026-01-08 19:54:15 +0100

    deployment fix #989 - import from react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter,Route,Routes} from "react-router";
+import {BrowserRouter,Route,Routes} from "react-router-dom";
 import CoinPage from './pages/coinPage.jsx'
 import NotFound from './pages/notFound.jsx'
 
